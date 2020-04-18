22bcf2b1408da0220c3c09f3ed666a6bce4b7c84
horse456 <cn45604@aliyun.com>
2020-04-19 00:19:06 +0800

    setup remenber the username and password

--- drawcircle.py
+++ drawcircle.py
@@ -28,18 +28,3 @@ for x in range(50):
     t.left(170)
 end_fill()
 time.sleep(2)
-
-#彩虹线
-
-# import turtle as t 
-# from random import randint as rint
-# t.shape("turtle")
-# t.pensize(5)
-# t.colormode(255)
-# t.bgcolor("black")
-# t.tracer(False)
-# for x in range(700):
-#     t.color(rint(0,255),rint(0,255),rint(0,255))
-#     t.circle(2*(1+x/4),5)
-#     t.speed(30)
-#     t.tracer(True)
