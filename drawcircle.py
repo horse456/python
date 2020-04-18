cbb62876e08f37517f6d569868f0c36519b5ae84
horse456 <cn45604@aliyun.com>
2020-04-18 21:18:23 +0800

    anthoner change

--- drawcircle.py
+++ drawcircle.py
@@ -1,22 +1,22 @@
 import math
 import turtle as t 
 
-# draw the circle using turtle
-# def drawCircleTurtle(x, y, r) :
-#     # move to the start of circle
-#     t.up()
-#     t.setpos(x + r, y)
-#     t.down()
+draw the circle using turtle
+def drawCircleTurtle(x, y, r) :
+    # move to the start of circle
+    t.up()
+    t.setpos(x + r, y)
+    t.down()
      
-#     #draw the circle
-#     for i in range (0, 365, 5):
-#          a = math.radians(i)
-#          t.setpos(x + r*math.cos(a), y + r*math.sin(a))
+    #draw the circle
+    for i in range (0, 365, 5):
+         a = math.radians(i)
+         t.setpos(x + r*math.cos(a), y + r*math.sin(a))
 
-# drawCircleTurtle(200, 100, 50)
-# t.mainloop()
+drawCircleTurtle(200, 100, 50)
+t.mainloop()
 
-# print("hello, world!")
+print("hello, world!")
 
 # 太阳线
 # import turtle as t
